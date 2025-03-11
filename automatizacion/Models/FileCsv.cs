77d3dbe997515b53818b0f785a8390f4aeab50d3
Tu Nombre <tu_correo@example.com>
2025-03-11 12:03:04 -0500

    first

---
+++ automatizacion/Models/FileCsv.cs
@@ -0,0 +1,10 @@
+﻿namespace automatizacion.Models
+{
+    public class FileCsv
+    {
+        public List<string> header { get; set; }
+        public string encabezado { get; set; }
+
+        public Dictionary<string, string> dividir { get; set; }
+    }
+}
